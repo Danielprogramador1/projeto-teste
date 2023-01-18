9a1a55dbbc84f0e599d2ea2a414b43e72b3d5722
Danielprogramador1 <122888291+Danielprogramador1@users.noreply.github.com>
2023-01-18 00:02:01 -0300

    Projeto-teste
    apenas um teste

---
+++ curriculo.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>curriculo</title>
+<meta charset="utf-8/>
+</head>
+Curriculo
+<hr>
+<body>
+    <form method="post"action="impresso.php">
+        nome: <input type="text" name="nome">
+        <Br><br>
+        Endereço: <input type="text" name="endereço">
+        <br><br>
+        Telefone: <input type="text" name="telefone">
+        <br><br>
+        Email: <input type="text" name="email">
+        <br><br>
+        <hr>
+        objetivo:
+        <textarea name="objetivo"></textarea>
+        <br><br>
+        formaçao<textarea name="formacao"></textarea>
+        <br><br>
+        experiencia proficional:<textarea name="experiencia"></textarea
+        <br><br>
+        informaçoes adicionais:<textarea name="informacoes"></textarea>
+        <br><br>
+        <input type="submit" name="submit"value="Enviar">
+</from>
+</body>
+</html>
---
+++ impresso.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>curriculo</title>
+<meta charset="utf-8/>
+</head>
+<body>
+<h1><?=$_POST['nome']?></h1>
+<hr>
+
+<?=$_POST['endereco']?>-<?=$_POST['telefone']?>-<?=$_POST['email']?>
+
+<h2>objetivo:</h2>
+
+<?=$_POST['objetivo']?>
+<br>
+<hr>
+
+<h2>formaçao:</h2>
+<?=$_POST['formacao']?>
+<br>
+<hr>
+
+<h2>Experiencia Profisional:</h2>
+<?=$_POST['experiencia']?>
+<br>
+<hr>
+
+<h2>Informaçoes Adicionais:</h2>
+<?=$_POST['informacoes']?>
+<br>
+</body>
+</html>
